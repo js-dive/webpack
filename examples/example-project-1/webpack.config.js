9e08fdf5076e34ddfc06af437f3ed692279bf5c6
gogoend <gogoend@qq.com>
2022-10-06 16:46:17 +0800

    build(gogoend): 去掉示例package.json中的webpack依赖，以便调试父目录中的webpack；重新生成yarn.lock；更改入口文件

--- examples/example-project-1/webpack.config.js
+++ examples/example-project-1/webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = [
 	{
 		entry: {
-			index: "./src/index.js"
+			index: "./example.js"
 		},
 		output: {
 			path: path.resolve(__dirname, "dist"),
