ffc08fe5fd0b58c58e5933e1654b11a13832e6ae
gogoend <gogoend@qq.com>
2023-01-06 11:11:22 +0800

    feat(example-project-1): 简化构建示例 - 仅构建js文件、仅保留最基本的webpack配置

--- examples/example-project-1/src/index.js
+++ examples/example-project-1/src/index.js
@@ -1,7 +1,7 @@
 import { logGogoend } from './modules/module-2'
 const { logHello } = require('./modules/module-1')
 
-const snapshotImg = require('./assets/image-1.png')
+// const snapshotImg = require('./assets/image-1.png')
 const examplePackageJson = require('./assets/json-1.json')
 
 export const someNumber = 666
--- examples/example-project-1/webpack.config.js
+++ examples/example-project-1/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = [
 	{
@@ -14,44 +14,44 @@ module.exports = [
 		},
 		mode: "production",
 		optimization: {
-			minimize: false,
-			concatenateModules: false,
-			splitChunks: {
-				chunks: "all"
-			}
-		},
-		module: {
-			rules: [
-				{
-					test: /\.png$/,
-					use: [
-						// {
-						// 	loader: 'raw-loader'
-						// },
-						{
-							loader: "file-loader",
-							options: {
-								name(file) {
-									if (process.env.NODE_ENV === "development") {
-										return "[path][name].[ext]";
-									}
+			minimize: false
+			// concatenateModules: false,
+			// splitChunks: {
+			// 	chunks: "all"
+			// }
+		}
+		// 	module: {
+		// 		rules: [
+		// 			{
+		// 				test: /\.png$/,
+		// 				use: [
+		// 					// {
+		// 					// 	loader: 'raw-loader'
+		// 					// },
+		// 					{
+		// 						loader: "file-loader",
+		// 						options: {
+		// 							name(file) {
+		// 								if (process.env.NODE_ENV === "development") {
+		// 									return "[path][name].[ext]";
+		// 								}
 
-									return "[hash].[ext]";
-								}
-							}
-						}
-					]
-				}
-			]
-		},
-		plugins: [
-			new CleanWebpackPlugin(),
-			// https://github.com/ampedandwired/html-webpack-plugin
-			new HtmlWebpackPlugin({
-				filename: "./index.html",
-				template: "./src/public/index.template.html",
-				inject: true
-			})
-		]
+		// 								return "[hash].[ext]";
+		// 							}
+		// 						}
+		// 					}
+		// 				]
+		// 			}
+		// 		]
+		// 	},
+		// 	plugins: [
+		// 		new CleanWebpackPlugin(),
+		// 		// https://github.com/ampedandwired/html-webpack-plugin
+		// 		new HtmlWebpackPlugin({
+		// 			filename: "./index.html",
+		// 			template: "./src/public/index.template.html",
+		// 			inject: true
+		// 		})
+		// ]
 	}
 ];
