ca11ff4e4f4b015c6ec5375f3a322b685850c9ac
gogoend <gogoend@qq.com>
2023-05-10 00:27:24 +0800

    chore(example-project-2-for-demonstrate-esm-import-dependency-relation): 在module-2中引入tslib依赖，以了解打包产物中有哪些内容

--- examples/example-project-2-for-demonstrate-esm-import-dependency-relation/src/modules/module-2.js
+++ examples/example-project-2-for-demonstrate-esm-import-dependency-relation/src/modules/module-2.js
@@ -1,4 +1,6 @@
 import { someNumber } from "../index"
 export * as module_3 from "./module-3"
+import * as tslib from 'tslib'
 
 export const logGogoend = () => console.log('gogoend', someNumber)
+console.log(tslib)
